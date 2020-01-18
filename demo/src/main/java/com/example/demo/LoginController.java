05d39a4fa48da59ddc0c5d92c2fde715ba53d482
kpagilla <46722572+kpagilla@users.noreply.github.com>
2020-01-18 17:16:19 -0600

    Added login and greeting page

--- demo/src/main/java/com/example/demo/LoginController.java
+++ demo/src/main/java/com/example/demo/LoginController.java
@@ -1,17 +1,12 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @RequestMapping(value = "/login")
+    public String index() {
+        return "login";
     }
-
 }
